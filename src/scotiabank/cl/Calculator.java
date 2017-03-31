eec8410688e01b39e38cc770b279478186f7d448
FELIPE MORALES <zerodump2@gmail.com>
2017-03-31 02:27:41 -0300

    correccionproyecto

--- src/scotiabank/cl/Calculator.java
+++ src/scotiabank/cl/Calculator.java
@@ -1,3 +1,4 @@
+package scotiabank.cl;
 
 public class Calculator {	
 	private String[][]  matriz = {
--- src/scotiabank/cl/CalculatorTest.java
+++ src/scotiabank/cl/CalculatorTest.java
@@ -1,3 +1,4 @@
+package scotiabank.cl;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
